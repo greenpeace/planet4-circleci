90ccb4b8218e4263bd591f9d5c9c573fd6bb79cc
Konstantinos Kokkorogiannis <koyandianthos@gmail.com>
2019-06-16 12:24:26 +0300

    -u flag and better way to pass password

--- src/circleci-base/scripts/docker-login.sh
+++ src/circleci-base/scripts/docker-login.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-set -eo pipefail
+set -euo pipefail
 
-docker login --username "$(echo "${DOCKER_USER_64}" | base64 --decode)" --password "$(echo "${DOCKER_PASS_64}" | base64 --decode)"
+echo "${DOCKER_PASS_64}" | base64 --decode | docker login --username "$(echo "${DOCKER_USER_64}" | base64 --decode)" --password-stdin
